704f854740f80beee58b0ec5fee6454e464f3e38
Meghana <sistla.meghana@gmail.com>
2023-05-22 23:41:27 -0500

    Adding DDPackage

---
+++ dd_package/CMakeFiles/_CMakeLTOTest-CXX/bin/CMakeFiles/foo.dir/foo.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/foo.dir/foo.cpp.o: \
+  /Users/meghana/Desktop/Meghana/Research/MQT_DD/dd_package/CMakeFiles/_CMakeLTOTest-CXX/src/foo.cpp
---
+++ dd_package/build/CMakeFiles/_CMakeLTOTest-CXX/bin/CMakeFiles/foo.dir/foo.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/foo.dir/foo.cpp.o: \
+  /Users/meghana/Desktop/Meghana/Research/MQT_DD/dd_package/build/CMakeFiles/_CMakeLTOTest-CXX/src/foo.cpp
